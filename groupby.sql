bf3cfab47637b2d056a11452163cd801b4eea18b
Serena <sere.michetti@icloud.com>
2026-03-09 21:04:38 +0100

    SVOLTO SECONDO ESERCIZIO SU GROUP BY

--- groupby.sql
+++ groupby.sql
@@ -4,6 +4,8 @@ FROM students
 GROUP BY year(ENROLMENT_DATE) 
 ORDER BY year(ENROLMENT_DATE);
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-
+SELECT count(*), office_address
+FROM teachers
+GROUP BY office_address;
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
